f8618c177c7de9943cb178bebd2d0ed5045c914b
OsvaldoPerezOchoa <l19141186@queretaro.tecnm.mx>
2025-04-09 11:46:44 -0600

    init project

---
+++ src/Variables.java
@@ -0,0 +1,25 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        int edad = 31;
+        double precio = 360.40;
+        boolean disponible = true;
+        char genero = 'M';
+
+        //Variable edad
+        System.out.println(edad);
+
+        //Modificar variable de edad
+        edad = 45;
+        System.out.println(edad);
+
+        //Imprimir variable precio
+        System.out.println(precio);
+
+        //Imprimir variable disponible
+        System.out.println(disponible);
+
+        //Imprimir variable de genero
+        System.out.println(genero);
+    }
+}
