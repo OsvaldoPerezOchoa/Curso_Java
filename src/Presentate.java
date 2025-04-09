bdda9b09ba0ffca8fa8c8a5b758c1643371f0230
OsvaldoPerezOchoa <l19141186@queretaro.tecnm.mx>
2025-04-09 11:16:46 -0600

    init project

---
+++ src/Presentate.java
@@ -0,0 +1,7 @@
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Nombre: Osvaldo Pérez Ochoa");
+        System.out.println("Edad: 24");
+        System.out.println("México");
+    }
+}
